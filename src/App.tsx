248754f48486db6adc07f2716786d1b0449442a5
Jason Nordheim <jason.nordheim@gmail.com>
2023-09-07 10:54:38 -0600

    refactored to use constants, added in useMount

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,18 @@ import { Instructions } from "./components/Instructions";
 import { Legend } from "./components/Legend";
 import "./App.css";
 import { DEFAULT_NODES_PER_ROW, initializeGraph, useAppDispatch, useAppSelector } from "./state";
-import { useLoadListener, useResizeListener } from "./hooks";
+import { useLoadListener, useMount, useResizeListener } from "./hooks";
 import { useState } from "react";
+import { WIDTH_THRESHOLDS } from "./common";
 
 const Styles: { [k: number]: React.CSSProperties } = {
-  [800]: {
+  [WIDTH_THRESHOLDS.LG]: {
     display: "grid",
     gridTemplateColumns: "50% 20% 30%",
     maxWidth: "1000px",
     margin: "0 auto",
   },
-  [500]: {
+  [WIDTH_THRESHOLDS.MD]: {
     display: "gird",
     gridTemplateColumns: "60% 40%",
     gridTemplateRows: "repeat(1, auto)",
@@ -28,18 +29,19 @@ function App() {
 
   const handleResize = () => {
     const width = window.innerWidth;
-    if (width >= 800 && nodesPerRow != DEFAULT_NODES_PER_ROW) {
-      setWidthThreshold(800);
+    if (width >= WIDTH_THRESHOLDS.LG && nodesPerRow != DEFAULT_NODES_PER_ROW) {
+      setWidthThreshold(WIDTH_THRESHOLDS.LG);
       dispatch(initializeGraph({ numPerRow: DEFAULT_NODES_PER_ROW }));
-    } else if (width < 800 && width > 500) {
-      setWidthThreshold(500);
+    } else if (width < WIDTH_THRESHOLDS.LG && width > WIDTH_THRESHOLDS.MD && nodesPerRow != 20) {
+      setWidthThreshold(WIDTH_THRESHOLDS.MD);
       dispatch(initializeGraph({ numPerRow: 20 }));
     }
     console.log({ windowWidth: width });
   };
 
-  useResizeListener(handleResize);
   useLoadListener(handleResize);
+  useResizeListener(handleResize);
+  useMount(handleResize);
 
   return (
     <div id="app">
@@ -50,7 +52,7 @@ function App() {
         <aside style={Styles[widthThreshold]}>
           <Instructions />
           <Legend />
-          <Controls />
+          <Controls threshold={widthThreshold} />
         </aside>
         <Grid />
         <aside></aside>
--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,4 +1,6 @@
-import { searchGraph, useAppDispatch, useAppSelector } from "../state";
+import { FC } from "react";
+import { initializeGraph, searchGraph, useAppDispatch, useAppSelector } from "../state";
+import { WIDTH_THRESHOLDS } from "../common";
 
 // const NumberOfNodes = () => {
 //   const itemsPerRow = useAppSelector((state) => state.itemsPerRow);
@@ -17,35 +19,50 @@ import { searchGraph, useAppDispatch, useAppSelector } from "../state";
 //   );
 // };
 
-export const Controls = () => {
+const Styles: { [k: number]: React.CSSProperties } = {
+  [WIDTH_THRESHOLDS.LG]: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    gap: "5px",
+    margin: "1em",
+  },
+  [WIDTH_THRESHOLDS.MD]: {
+    columnSpan: "all",
+  },
+};
+
+export const Controls: FC<{ threshold: number }> = ({ threshold }) => {
   const dispatch = useAppDispatch();
   const start = useAppSelector((state) => state.start);
   const end = useAppSelector((state) => state.end);
   const status = useAppSelector((state) => state.status);
+  const nodesPerRow = useAppSelector((state) => state.itemsPerRow);
 
   const handleRun = () => {
     dispatch(searchGraph());
   };
 
+  const handleReset = () => {
+    dispatch(initializeGraph({ numPerRow: nodesPerRow }));
+  };
+
   return (
-    <div
-      id="controls"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        gap: "5px",
-        margin: "1em",
-        fontFamily: "sans-serif",
-      }}
-    >
+    <div id="controls" style={Styles[threshold]}>
       <h4 style={{ textAlign: "center" }}>Controls</h4>
       <button
         disabled={(!start && !end) || status == "working"}
         onClick={handleRun}
         aria-label="find the shortest path"
       >
-        Find shortest path
+        Find Path
+      </button>
+      <button
+        disabled={(!start && !end) || status == "working"}
+        onClick={handleReset}
+        aria-label="find the shortest path"
+      >
+        Reset
       </button>
     </div>
   );
