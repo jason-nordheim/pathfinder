f9a9564733d463fe44ff84cfb85996bceb66118f
Jason Nordheim <jason.nordheim@gmail.com>
2023-09-07 10:32:51 -0600

    setup logic for changing the grid on devices larger than 500 px wide

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,42 @@ import { Instructions } from "./components/Instructions";
 import { Legend } from "./components/Legend";
 import "./App.css";
 import { DEFAULT_NODES_PER_ROW, initializeGraph, useAppDispatch, useAppSelector } from "./state";
-import { useResizeListener } from "./hooks";
+import { useLoadListener, useResizeListener } from "./hooks";
+import { useState } from "react";
+
+const Styles: { [k: number]: React.CSSProperties } = {
+  [800]: {
+    display: "grid",
+    gridTemplateColumns: "50% 20% 30%",
+    maxWidth: "1000px",
+    margin: "0 auto",
+  },
+  [500]: {
+    display: "gird",
+    gridTemplateColumns: "60% 40%",
+    gridTemplateRows: "repeat(1, auto)",
+  },
+};
 
 function App() {
+  const [widthThreshold, setWidthThreshold] = useState(800);
   const nodesPerRow = useAppSelector((state) => state.itemsPerRow);
   const dispatch = useAppDispatch();
 
   const handleResize = () => {
-    const windowWidth = window.innerWidth;
-    if (windowWidth > 800 && nodesPerRow != DEFAULT_NODES_PER_ROW) {
+    const width = window.innerWidth;
+    if (width >= 800 && nodesPerRow != DEFAULT_NODES_PER_ROW) {
+      setWidthThreshold(800);
       dispatch(initializeGraph({ numPerRow: DEFAULT_NODES_PER_ROW }));
+    } else if (width < 800 && width > 500) {
+      setWidthThreshold(500);
+      dispatch(initializeGraph({ numPerRow: 20 }));
     }
-    console.log({ windowWidth });
+    console.log({ windowWidth: width });
   };
 
-  useResizeListener({ onResize: handleResize });
+  useResizeListener(handleResize);
+  useLoadListener(handleResize);
 
   return (
     <div id="app">
@@ -26,14 +47,13 @@ function App() {
         <h1>Pathfinder</h1>
       </div>
       <div id="app-content">
-        <aside style={{ display: "inline-grid", gridTemplateColumns: "repeat(2, 1fr)" }}>
+        <aside style={Styles[widthThreshold]}>
           <Instructions />
           <Legend />
-        </aside>
-        <Grid />
-        <aside>
           <Controls />
         </aside>
+        <Grid />
+        <aside></aside>
       </div>
     </div>
   );
